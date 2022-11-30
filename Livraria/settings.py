584bbe988b2d61bd137f16559de043d304a6aef2
Guilherme Gamaliel Lima <111901785+GuilhermeGamaliel@users.noreply.github.com>
2022-11-30 14:33:13 -0300

    Update settings.py

--- Livraria/settings.py
+++ Livraria/settings.py
@@ -19,18 +19,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w3636v1#z04)9^2!v(%so)ivhv-i27h27-)(j5le_-vnw8qid='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
-
 # Carrega as variáveis de ambiente do sistema operacional e as prepara para usá-las
 env = environ.Env()
 environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
@@ -99,17 +87,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Livraria.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
